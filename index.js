68b20a32f46686c10e027326c63011e912bb4f01
Franck <contact@franckwebpro.com>
2024-03-20 21:54:08 +0100

    rentrer des taches dans un tableau puis affichage de chaque élément du tableau, à terminer

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
-const todolist = document.getElementById('todo');
+const todoList = document.getElementById('todoList');
+const doneList = document.getElementById('doneList');
 const addButton = document.getElementById('addButton');
+const todo = [];
+const done = [];
 
-const itemAdded = () => {
+const createTrashButton = (parent) => {
+    const trashContainer = document.createElement('div');
+    const trashImg = document.createElement('img');
+    trashImg.src = 'images/trash.png';
+    trashImg.classList.add('trashImg');
+    trashContainer.classList.add('trashContainer');
+    trashContainer.appendChild(trashImg);
+    return parent.appendChild(trashContainer);
+}
+
+const createTaskItem = (newTask) => {
+    const li = document.createElement('li');
+    const input = document.createElement('input');
+    const label = document.createElement('label');
+    input.type = 'checkbox';
+    li.appendChild(input);
+    li.appendChild(label);
+    createTrashButton(li);
+    label.innerText = newTask;
+    return li;
+}
+
+const createNewTask = () => {
     addButton.addEventListener('click', () => {
-        const newItem = document.createElement('li');
-        const newTask = document.getElementById('task').value;
-        newItem.innerText = newTask;
-        todolist.appendChild(newItem);
+        if (document.getElementById('newTaskInput').value !== "") {
+            const newTask = document.getElementById('newTaskInput').value;
+            todo.push(newTask);
+        }
+        todoList.innerHTML = "";
+        for (let task of todo) {
+            createTaskItem(task);
+        }
+
     });
 };
 
-itemAdded();
+createNewTask();
+
+
+// const trashButton = document.querySelector('.trashContainer');
+
+// const deleteTask = () => {
+//     trashButton.forEach(button =>
+//         button.addEventListener('click', () => {
+//             const element = button.parentElement("div-02");
+//         }));
+// };
