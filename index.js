a275e89fadda8db80b72cedb577e4cfae7b55716
Franck <contact@franckwebpro.com>
2024-03-27 21:26:16 +0100

    finalisation de la todo

--- index.js
+++ index.js
@@ -23,15 +23,25 @@ const createTrashButton = (parent, index) => {
     return parent.appendChild(trashContainer);
 }
 
-//création de l'élément tâche et ajout du contenu
+//création de l'élément tâche et ajout du contenu (task) et de l'input checkbox
 const createTaskLine = (task, index) => {
     const li = document.createElement('li');
-    const input = document.createElement('input');
+    const checkbox = document.createElement('input');
     const span = document.createElement('span');
-    input.classList.add('taskCheckBox');
-    input.type = 'checkbox';
+    checkbox.classList.add('taskCheckBox');
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('change', () => {
+        const liToCheck = document.querySelector(`li[data-index='${index}']`);
+        if (checkbox.checked) {
+            liToCheck.remove();
+            doneList.appendChild(liToCheck);
+        } else {
+            liToCheck.remove();
+            todoList.appendChild(liToCheck);
+        }
+    });
     li.setAttribute('data-index', index);
-    li.appendChild(input);
+    li.appendChild(checkbox);
     li.appendChild(span);
     createTrashButton(li, index);
     span.innerText = task;
@@ -46,7 +56,7 @@ const createTaskItem = () => {
         if (newTaskValue !== "") {
             let itemIndex = localStorage.length + 1;
             const newTask = createTaskLine(newTaskValue, `listItem${itemIndex}`);
-            localStorage.setItem(`listItem${itemIndex}`, newTaskValue);
+            localStorage.setItem(`listItem${itemIndex}`, JSON.stringify({ value: newTaskValue, done: false }));
             todoList.appendChild(newTask);
             inputNewtask.value = "";
         }
@@ -59,13 +69,21 @@ const retrieveTaskItem = () => {
     const itemObject = {};
     const storageLength = localStorage.length;
     for (let i = 1; i <= storageLength; i++) {
-        let task = localStorage.getItem(`listItem${i}`);
-        let taskKey = `listItem${i}`;
-        itemObject[taskKey] = `${task}`;
-        const newTask = createTaskLine(`${task}`, `listItem${i}`);
-        todoList.appendChild(newTask);
+        let task = JSON.parse(localStorage.getItem(`listItem${i}`));
+        if (task.done === false) {
+            let taskKey = `listItem${i}`;
+            itemObject[taskKey] = `${task}`;
+            const newTask = createTaskLine(`${task.value}`, `listItem${i}`);
+            todoList.appendChild(newTask);
+        } else {
+            let taskKey = `listItem${i}`;
+            itemObject[taskKey] = `${task}`;
+            const newTask = createTaskLine(`${task.value}`, `listItem${i}`);
+            doneList.appendChild(newTask);
+        }
     }
 }
+
 
 createTaskItem();
 retrieveTaskItem();
